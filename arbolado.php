3e381fe34c44e01fa5a99f695f76b652b88e4035
Inma <inmaculada.oliasalvarez@iesjacaranda.es>
2024-10-08 12:32:14 +0200

    avanzando arbolado

--- arbolado.php
+++ arbolado.php
@@ -1,13 +1,32 @@
 <?php 
         session_start();
+        $msg=[];
         $words = ["manzana", "perro", "casa", "libro", "sol", "luna", "árbol", "flor", "mesa", "gato"];
         if (!isset($_SESSION["word"]) ||!isset($_GET["submit"])){
             $num = random_int(0,9);
             $_SESSION["word"]= $words[$num];
-            echo $_SESSION["word"];
+            $msg="Empezamos. Adivina mi palabra";
+            $error = 0;
+            $_SESSION["wordToShow"]= "";
+            for ($i = 0; $i < strlen($_SESSION["word"]); $i++){
+                $_SESSION["wordToShow"]= $_SESSION["wordToShow"] . "_ "; 
+            }
+            
         }else{
+           if (isset($_GET["letterInput"]) && strlen($_GET["letterInput"])== 1){
+            for ($i = 0; $i < $_SESSION["word"]; $i++){
+                if ($_SESSION["word"][$i] = $_GET["letterInput"]){
+                    $_SESSION["wordToShow"][$i] = $_GET["letterInput"];
+                } 
+            }
+           }else{
+            $msg="Debes introducir solo un carácter";
+           }
+
+
 
         }
+        echo $_SESSION["word"];
     ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -31,8 +50,8 @@
                             
                             <p id="word" class="fs-4" ><!-- Aquí se mostrarán los guiones -->
                             <?php 
-                                for ($i=0;$i < strlen($_SESSION["word"]);$i++){
-                                    echo "_ ";
+                                for ($i=0;$i < strlen($_SESSION["wordToShow"]);$i++){
+                                    echo $_SESSION["wordToShow"][$i] . " ";
                                 }
                             ?>
                            </p> 
@@ -40,10 +59,10 @@
                         <form id="guessForm">
                             <div class="mb-3">
                                 <label for="letterInput" class="form-label">Introduce una letra</label>
-                                <input type="text" class="form-control" id="letterInput" maxlength="1" required>
+                                <input type="text" class="form-control" name="letterInput" id="letterInput" maxlength="1" required>
                             </div>
                             <div class="d-grid">
-                                <button type="submit" class="btn btn-primary">Adivinar Letra</button>
+                                <button type="submit" value="" id="submit" name="submit" class="btn btn-primary">Adivinar Letra</button>
                             </div>
                         </form>
                         <div id="message" class="mt-3 text-center"></div>
